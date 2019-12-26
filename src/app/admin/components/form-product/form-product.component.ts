bbf462445c2507673bc4041e9a4dc285aec72b6d
Nahuel Bentos <nahuel.bentos@fing.edu.uy>
2019-12-26 13:30:43 -0300

    terminando video 54

--- src/app/admin/components/form-product/form-product.component.ts
+++ src/app/admin/components/form-product/form-product.component.ts
@@ -2,8 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { MyValidators } from './../../../utils/validators';
+import { AngularFireStorage } from '@angular/fire/storage';
 
+import { finalize } from 'rxjs/operators';
 import { ProductsService } from './../../../core/services/products/products.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-form-product',
@@ -13,11 +16,12 @@ import { ProductsService } from './../../../core/services/products/products.serv
 export class FormProductComponent implements OnInit {
 
   form: FormGroup;
-
+  image$: Observable<any>;
   constructor(
     private formBuilder: FormBuilder,
     private productsService: ProductsService,
-    private router: Router
+    private router: Router,
+    private storage: AngularFireStorage
   ) {
     this.buildForm();
   }
@@ -37,6 +41,27 @@ export class FormProductComponent implements OnInit {
     }
   }
 
+  uploadFile(event) {
+    const file = event.target.files[0];
+    console.log(file);
+    const name = 'image.jpg';
+    const fileRef = this.storage.ref(name);
+    const task = this.storage.upload(name, file);
+
+    task.snapshotChanges()
+      .pipe(
+        finalize(() => {
+          this.image$ = fileRef.getDownloadURL();
+          this.image$.subscribe(url => {
+            console.log(url);
+            this.form.get('image').setValue(url);
+          });
+        })
+      )
+      .subscribe();
+
+  }
+
   private buildForm() {
     this.form = this.formBuilder.group({
       id: ['', [Validators.required]],
@@ -50,5 +75,4 @@ export class FormProductComponent implements OnInit {
   get priceField() {
     return this.form.get('price');
   }
-
 }
