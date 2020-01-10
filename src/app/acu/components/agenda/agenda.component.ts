3f6be4f94f9612985eccb2c85b38ed6ed2c94c3b
Nahuel Bentos <nahuel.bentos@fing.edu.uy>
2020-01-10 11:18:32 -0300

    Quedo pronta la primera versión de la agenda

--- src/app/acu/components/agenda/agenda.component.ts
+++ src/app/acu/components/agenda/agenda.component.ts
@@ -19,8 +19,32 @@ export interface Horas {
   }];
 }
 export interface AgendaElement {
-  Hora: string;
-  col: any[];
+  Movil: string;
+  // Hora0: string;
+  Hora1: string;
+  Hora2: string;
+  Hora3: string;
+  Hora4: string;
+  Hora5: string;
+  Hora6: string;
+  Hora7: string;
+  Hora8: string;
+  Hora9: string;
+  Hora10: string;
+  Hora11: string;
+  Hora12: string;
+  Hora13: string;
+  Hora14: string;
+  Hora15: string;
+  Hora16: string;
+  Hora17: string;
+  Hora18: string;
+  Hora19: string;
+  Hora20: string;
+  Hora21: string;
+  Hora22: string;
+  Hora23: string;
+  Hora24: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
@@ -44,11 +68,11 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class AgendaComponent implements OnInit {
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  agendaDisplayedColumns: string[] = ['Hora'];
+  agendaDisplayedColumns: string[] = ['Movil'];
   columns: string[] = [];
   rows: string[] = [];
   dataSource = ELEMENT_DATA;
-  agendaDataSource: any = null;
+  agendaDataSource: AgendaElement[] = [];
   agenda: any[] = [];
   moviles: any[] = [];
   horas: Horas[] = [];
@@ -64,12 +88,13 @@ export class AgendaComponent implements OnInit {
         this.moviles = res.TablaAgenda.Moviles;
         this.horas = res.TablaAgenda.Horas;
         console.log('2)horas: ', this.horas);
-        this.columns = this.moviles.map(item => item.MovCod.toString());
+        this.columns = this.horas.map(item => item.Hora.toString()); // this.moviles.map(item => item.MovCod.toString());
         this.rows = this.horas.map(item => item.Hora.toString());
 
         this.agendaDataSource = this.makeDataSource(this.columns, this.horas, this.moviles, res.TablaAgenda.HoraMovilPlano);
 
         this.agendaDisplayedColumns = this.agendaDisplayedColumns.concat(this.columns);
+        console.log('agendaDisplayedColumns: ', this.agendaDisplayedColumns);
 
       });
   }
@@ -87,32 +112,57 @@ export class AgendaComponent implements OnInit {
     console.log('horasMoviles: ', moviles);
 
 
+    // let i = 0;
+    // let j = 0;
+    // for (const h of horas) {
+
+    //   const o = {};
+    //   // tslint:disable-next-line: no-string-literal
+    //   o['Movil'] = h.Hora;
+    //   let valueI = 0
+    //   for (const m of moviles) {
+    //     let val = '';
+
+    //     if (this.existeEnHorasMoviles(h, m, horasMoviles)) {
+    //       i++;
+    //       val = i.toString() + ' ' + m.Valor;
+    //     } else {
+    //       j--;
+    //       val = j.toString() + ' ';
+    //     }
+    //     o['value' + valueI] = val;
+    //     valueI++;
+    //     //o[m.MovCod] = val;
+    //   }
+    //   console.log('object: ', o);
+    //   col.push(o);
+
+    // }
+
     let i = 0;
     let j = 0;
-    for (const h of horas) {
+    for (const m of moviles) {
 
       const o = {};
       // tslint:disable-next-line: no-string-literal
-      o['Hora'] = h.Hora;
-      let valueI = 0
-      for (const m of moviles) {
-        let val = '';
+      o['Movil'] = m.MovCod;
 
-        if (this.existeEnHorasMoviles(h, m, horasMoviles)) {
-          i++;
-          val = i.toString() + ' ' + m.Valor;
-        } else {
+      for (const h of horas) {
+        let val = this.existeEnHorasMoviles(h, m, horasMoviles);
+        if (val === '') {
           j--;
-          val = j.toString() + ' ';
+          val = 'L'; // j.toString() + ' ';
+        } else {
+          i++;
+          val = 'O'; // i.toString() + ' ' + val;
         }
-        o['value' + valueI] = val;
-        valueI++;
-        //o[m.MovCod] = val;
+        o['Hora' + h.Hora] = val;
       }
+
       console.log('object: ', o);
       col.push(o);
-
     }
+
     console.log('col: ', col);
 
     return col;
@@ -124,12 +174,15 @@ export class AgendaComponent implements OnInit {
 
     for (const h of horaMovilPlano) {
       if (h.Hora === hora.Hora && h.MovCod === movil.MovCod) {
-        return true;
+        return h.AluId;
       }
     }
 
-    return false;
+    return '';
 
   }
 
+  showAlert() {
+    alert('Hola');
+  }
 }
