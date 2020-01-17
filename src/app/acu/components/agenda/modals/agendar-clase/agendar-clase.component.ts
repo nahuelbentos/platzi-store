cf7135883075f31825f9065578954baf600997e4
Nahuel Bentos <nahuel.bentos@fing.edu.uy>
2020-01-17 11:33:53 -0300

    Test campos deshabilitados en modal

--- src/app/acu/components/agenda/modals/agendar-clase/agendar-clase.component.ts
+++ src/app/acu/components/agenda/modals/agendar-clase/agendar-clase.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AgendaComponent } from '../../agenda.component';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MyValidators } from '@utils/validators';
 
 
 
@@ -12,17 +14,27 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class AgendarClaseComponent implements OnInit {
 
+  form: FormGroup;
   selected = ' ';
   agendaClase: AgendaClase;
   hora: Date = new Date();
   fechaClase: Date = new Date();
+  movil: number;
   instructorAsignado = '';
   curso = '';
 
   // constructor() { }
   constructor(
+    private formBuilder: FormBuilder,
     public dialogRef: MatDialogRef<AgendaComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any) { }
+    @Inject(MAT_DIALOG_DATA) public data: any) {
+
+    this.agendaClase = this.data.agendaClase;
+    this.hora.setHours(this.agendaClase.Hora, 0);
+    this.instructorAsignado = `${this.agendaClase.EsAgCuInsId.toString().trim()} ${this.agendaClase.EsAgCuInsNom}`;
+    this.movil = this.agendaClase.EscMovCod;
+    this.buildForm();
+  }
 
   onNoClick(): void {
     this.dialogRef.close();
@@ -30,10 +42,59 @@ export class AgendarClaseComponent implements OnInit {
 
   ngOnInit() {
     console.log('data: ', this.data);
-    this.agendaClase = this.data.agendaClase;
-    this.hora.setHours(this.agendaClase.Hora, 0);
-    this.instructorAsignado = `${this.agendaClase.EsAgCuInsId.toString().trim()} ${this.agendaClase.EsAgCuInsNom}`;
 
+
+  }
+
+
+  private buildForm() {
+    console.log('agenda from buildForm: ', this.agendaClase);
+    if (this.agendaClase) {
+      this.form = this.formBuilder.group({
+        fechaClase: [this.agendaClase.FechaClase],
+        movil: [this.agendaClase.EscMovCod, [Validators.required]],
+        alumnoNumero: [this.agendaClase.AluNro],
+        alumnoNombre: [this.agendaClase.AluNomApe],
+        curso: [this.curso],
+        tipoClase: [this.agendaClase.EsAgCuTipCla],
+        numeroClase: [this.agendaClase.EsAgCuNroCla],
+        claseAdicional: [this.agendaClase.EsAgCuClaAdiSN],
+        avisoInstructor: [this.agendaClase.AvisoInstructor],
+        instructorAsignado: [this.instructorAsignado],
+        detalle: [this.agendaClase.EsAgCuDet],
+        estadoClase: [this.agendaClase.EsAgCuEst],
+        observaciones: [this.agendaClase.EsAgCuObs],
+        aviso: [this.agendaClase.EsAgCuDetAviso],
+      });
+
+    } else {
+      this.form = this.formBuilder.group({
+        fechaClase: ['', [Validators.required]],
+        movil: ['', [Validators.required]],
+        alumnoNumero: [''],
+        alumnoNombre: [''],
+        curso: [''],
+        tipoClase: [''],
+        numeroClase: [''],
+        claseAdicional: [''],
+        avisoInstructor: [''],
+        instructorAsignado: [''],
+        detalle: [''],
+        estadoClase: [''],
+        observaciones: [''],
+        aviso: [''],
+      });
+    }
+  }
+
+
+  guardarClase(event: Event) {
+    event.preventDefault();
+    if (this.form.valid) {
+      const agendaClase = this.form.value;
+
+
+    }
   }
 
 }
